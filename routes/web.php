858c0ffb05ae4b4967724761b9cbd72c151d44b5
kangajos <aguss6460@gmail.com>
2020-12-29 14:24:37 +0700

    route auth login and register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.backend.dashboard.index');
+});
+
+// route login and register
+Route::prefix("auth")->group(function () {
+    Route::get("/", [AuthController::class, "login"])->name("auth.login");
+    Route::get("/register", [AuthController::class, "register"])->name("auth.register");
 });
